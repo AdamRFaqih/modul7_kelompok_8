b40707030ce2b158790f88a2fbfb4b9a1703600d
adam <>
2023-04-04 15:42:25 +0700

    menambahkan class TeamMembers1302210008

--- Modul7_kelompok_8/Program.cs
+++ Modul7_kelompok_8/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Modul7_kelompok_8;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        TeamMembers1302210008 tim = new TeamMembers1302210008();
+        tim.readJSON();
+    }
+}
