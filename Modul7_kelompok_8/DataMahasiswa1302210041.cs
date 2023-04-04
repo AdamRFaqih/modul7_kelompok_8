3e81ac51bfe3055f512ae5222fa2a304817fd282
ecospheree <prayoganabiel74@gmail.com>
2023-04-04 16:55:06 +0700

    menambahkan class
    datamahasiswa

---
+++ Modul7_kelompok_8/DataMahasiswa1302210041.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Modul7_kelompok_8
+{
+    public class DataMahasiswa1302210041
+    {
+        Mahasiswa mahasiswa = new Mahasiswa();
+        String jsonString = File.ReadAllText(@"./jurnal7_1_1302210041.Json");
+
+        public void readJSON()
+        {
+            mahasiswa = JsonSerializer.Deserialize<Mahasiswa>(jsonString);
+            Console.WriteLine($"Nama {mahasiswa.firstName + " " + mahasiswa.lastName} dengan gender {mahasiswa.gender} " +
+                $"dengan umur {mahasiswa.age} ,beralamat {mahasiswa.address.streetAddress + " " + mahasiswa.address.city +
+                " " + mahasiswa.address.state}");
+            for (int i = 0; i < mahasiswa.courses.Count; i++)
+            {
+                Console.WriteLine($"<{mahasiswa.courses[i].code}> - <{mahasiswa.courses[i].name}>");
+            }
+        }
+    }
+}
--- Modul7_kelompok_8/Program.cs
+++ Modul7_kelompok_8/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+namespace Modul7_kelompok_8
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            DataMahasiswa1302210041 data = new DataMahasiswa1302210041();
+            data.readJSON();
+        }
+    }
+}
