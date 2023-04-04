df6e242039aeef85bec82a522d460a8cc0dc2020
MaximusBayu <121425228+MaximusBayu@users.noreply.github.com>
2023-04-04 16:55:15 +0700

    menambahkan class DataMahasiswa1302210101

--- Modul7_kelompok_8/Program.cs
+++ Modul7_kelompok_8/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Modul7_kelompok_8;
+
+DataMahasiswa1302210101 max = new DataMahasiswa1302210101();
+max.readJSON();
